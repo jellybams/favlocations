d5198d822f2a15b0a9948369d8e65a844e788533
Spoolphiz <alexander.manea@gmail.com>
2014-03-01 14:01:11 -0800

    removing config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	development : {
-		db: 'mongodb://localhost/favlocations',
-	},
-
-	production : {
-		db: 'mongodb://spoolphiz:testingstuff@ds033499.mongolab.com:33499/favlocations',
-	}
-};
